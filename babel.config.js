ac32f6694c9a172be96b611bd094c4373e65c50d
Mitch Downey <mitchldowney@gmail.com>
2021-10-09 18:38:16 -0500

    Bump react-native-reanimated to 2.3.0-beta.2

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
+
 module.exports = {
-    presets: ['module:metro-react-native-babel-preset']
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.tsx', '.ts', '.js', '.json']
+      },
+    ],
+    'react-native-reanimated/plugin'
+  ]
 };
